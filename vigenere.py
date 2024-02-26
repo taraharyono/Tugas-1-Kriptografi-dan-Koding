8e88f69671ae8a38ad47c4f77b14909616232009
taraharyono <103413692+taraharyono@users.noreply.github.com>
2024-02-26 22:27:11 +0700

    fix: extended vigenere bug

--- vigenere.py
+++ vigenere.py
@@ -1,10 +1,11 @@
 def extendedVigenereEncrypt(word, key):
     k = generateKey(word, key)
+    processed_word = word.replace(" ", "")
     result = ""
-    for i in range(len(word)):
-        c = (ord(word[i]) + ord(k[i])) % 256
+    for i in range(len(processed_word)):
+        c = (ord(processed_word[i]) + ord(k[i])) % 256
         result += chr(c)
-    print(result)
+    return result
 
 def extendedVigenereDecrypt(word, key):
     k = generateKey(word, key)
@@ -12,7 +13,7 @@ def extendedVigenereDecrypt(word, key):
     for i in range(len(word)):
         c = (ord(word[i]) - ord(k[i])) % 256
         result += chr(c)
-    print(result)
+    return result
 
 def generateKey(word, k):
     length = len(k)
